49093349ccf0b8fecfb981b99ad9d052a95ed5d4
mookoid <108244944+mookoid@users.noreply.github.com>
2023-03-17 11:24:58 +1100

    Update loading.js

--- web/pages/loading.js
+++ web/pages/loading.js
@@ -10,7 +10,7 @@ import Loading from '../components/Loading';
 type Props = {};
 
 // NOTE: I built a Loading component a while ago for Flatris, but later
-// realized it wasn't needed since everything was so fast. So I made a page
+// realized it wasn't needed since everything was so fast. So I made a page (yep)
 // to be able to look at it ¯\_(ツ)_/¯
 class LoadingPage extends Component<Props> {
   static async getInitialProps({ req, store }) {
